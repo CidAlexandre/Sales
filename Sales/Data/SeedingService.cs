905d7afb4a1687445f05788b206c4543fecda063
CidAlexandre <alexandrefariacid@gmail.com>
2022-01-23 15:10:48 -0300

    add sallers controller

--- Sales/Data/SeedingService.cs
+++ Sales/Data/SeedingService.cs
@@ -117,7 +117,7 @@ namespace Sales.Data
                 r21, r22, r23, r24, r25, r26, r27, r28, r29, r30
                 );
 
-            _context.SaveChanges();s
+            _context.SaveChanges();
         }
     }
 }
